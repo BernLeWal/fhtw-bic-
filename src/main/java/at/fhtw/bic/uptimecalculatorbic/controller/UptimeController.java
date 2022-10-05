185c386b865b4a3fb1470838bdffc63a7594c696
wallisch <walliscb@technikum-wien.at>
2022-10-05 20:24:13 +0200

    add uptime calculations in hours

--- src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/UptimeController.java
+++ src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/UptimeController.java
@@ -6,10 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UptimeController {
-    @GetMapping("/uptime")
-    public double calculateUptime(@RequestParam double relative) {
+    @GetMapping("/uptime/minutes")
+    public double calculateUptimeMinutes(@RequestParam double relative) {
         double minutesOfMonth = 60*24*30;
         double definedUptimeInPercent = relative / 100;
         return minutesOfMonth * definedUptimeInPercent; // uptime in minutes
     }
+
+    @GetMapping("/uptime/hours")
+    public double calculateUptimeHours(@RequestParam double relative) {
+        double hoursOfMonth = 24*30;
+        double definedUptimeInPercent = relative / 100;
+        return hoursOfMonth * definedUptimeInPercent;   // uptime in hours
+    }
+
 }

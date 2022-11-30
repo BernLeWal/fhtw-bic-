85e5c07e4b032e215799e9a791ff922ccaf237a3
wallisch <walliscb@technikum-wien.at>
2022-11-30 20:09:04 +0100

    impl. DowntimeController (without REST)

--- src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/DowntimeController.java
+++ src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/DowntimeController.java
@@ -2,8 +2,14 @@ package at.fhtw.bic.uptimecalculatorbic.controller;
 
 import jdk.jshell.spi.ExecutionControl;
 
+import java.security.InvalidParameterException;
+
 public class DowntimeController {
-    public double calculateDowntimeMinutes(double v) {
-        return 0.0;
+    public double calculateDowntimeMinutes(double relative) {
+        if (relative<0.0 || relative > 100.0)
+            throw new InvalidParameterException();
+        double minutesOfMonth = 60*24*30;
+        double definedUptimeInPercent = relative / 100;
+        return minutesOfMonth * definedUptimeInPercent; // uptime in minutes
     }
 }

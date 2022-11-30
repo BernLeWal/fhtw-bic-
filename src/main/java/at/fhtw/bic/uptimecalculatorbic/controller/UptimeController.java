583fb055f26eae24a69b2e5b43d9a2421f1883dd
wallisch <walliscb@technikum-wien.at>
2022-11-30 18:54:54 +0100

    add uptime calculations in hours

--- src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/UptimeController.java
+++ src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/UptimeController.java
@@ -10,7 +10,7 @@ public class UptimeController {
     public double calculateUptimeMinutes(@RequestParam double relative) {
         double minutesOfMonth = 60*24*30;
         double definedUptimeInPercent = relative / 100;
-        return minutesOfMonth * definedUptimeInPercent; // uptime in minutes
+        return -minutesOfMonth * definedUptimeInPercent; // uptime in minutes
     }
 
     @GetMapping("/uptime/hours")
---
+++ src/test/java/at/fhtw/bic/uptimecalculatorbic/controller/UptimeControllerTest.java
@@ -0,0 +1,24 @@
+package at.fhtw.bic.uptimecalculatorbic.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UptimeControllerTest {
+
+    @Test
+    void calculateUptimeMinutes() {
+        // A - Arrange
+        UptimeController controller = new UptimeController();
+
+        // A - Act
+        var result = controller.calculateUptimeMinutes(0.0);
+
+        // A - Assert
+        assertEquals(0.0, result);
+    }
+
+    @Test
+    void calculateUptimeHours() {
+    }
+}
